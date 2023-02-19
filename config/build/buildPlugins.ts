e3c29af7192b7759cb9c697165429449dccc7f15
Philipp Zakharov <philippzakharov@gamil.com>
2023-02-19 19:52:10 +0300

    Add webpack-bundle-analyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,10 +1,11 @@
 import path from 'path';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import ESLintWebpackPlugin from 'eslint-webpack-plugin';
-import ForkTsCheckerWebpackPlugin from'fork-ts-checker-webpack-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({
@@ -22,16 +23,24 @@ export function buildPlugins({
       chunkFilename: 'css/[name].[contenthash].css',
     }),
     isDev && new ReactRefreshWebpackPlugin(),
-    isDev && new ForkTsCheckerWebpackPlugin({
-      typescript: {
-        configFile: path.resolve(root, "tsconfig.json"),
-        diagnosticOptions: { syntactic: true, semantic: true, declaration: true, global: true },
-        mode: "write-references",
-      },
-    }),
-    isDev && new ESLintWebpackPlugin({
-      overrideConfigFile: path.resolve(root, '.eslintrc.json'),
-      extensions: ['js', 'jsx', 'ts', 'tsx'],
-    })
+    isDev &&
+      new ForkTsCheckerWebpackPlugin({
+        typescript: {
+          configFile: path.resolve(root, 'tsconfig.json'),
+          diagnosticOptions: {
+            syntactic: true,
+            semantic: true,
+            declaration: true,
+            global: true,
+          },
+          mode: 'write-references',
+        },
+      }),
+    isDev &&
+      new ESLintWebpackPlugin({
+        overrideConfigFile: path.resolve(root, '.eslintrc.json'),
+        extensions: ['js', 'jsx', 'ts', 'tsx'],
+      }),
+    isDev && new BundleAnalyzerPlugin(),
   ].filter(Boolean);
 }
